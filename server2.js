b390ef43e50db41a83e35bb23297d4efed43f508
mdsiaofficial <mdsiaofficial@gmail.com>
2024-06-30 20:21:50 +0600

    "Refactor server code to use middleware functions for logging and JSON response, and separate route handlers for GET /api/users and GET /api/users/:id"

--- server2.js
+++ server2.js
@@ -7,36 +7,66 @@ const users = [
   { id: 4, name: 'Baba Yaga' },
 ];
 
-const server = createServer((req, res) => {
-  if (req.url === '/api/users' && req.method === 'GET') {
-    res.setHeader('Content-Type', 'application/json');
-    res.write(JSON.stringify(users));
-    res.end();
+// Logger Middleware
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+}
 
-  } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
+// JSON middleware
+const jsonMiddleware = (req, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
+  next();
+}
 
-    const id = req.url.split('/')[3];
-    // console.log(id);
-    const user = users.find((user) => user.id === parseInt(id));
-    if (user) {
-      res.setHeader('Content-Type', 'application/json');
-      res.write(JSON.stringify(user));
-      res.end();
-    } else {
-      res.setHeader('Content-Type', 'application/json');
-      res.statusCode = 404;
-      res.write(JSON.stringify({ message: `User ${id} not found` }));
-      res.end();
-    }
+// Route handler for GET /api/users
+const getUserHandler = (req, res) => {
+  res.write(JSON.stringify(users));
+  res.end();
+}
 
+// Route handler for GET /api/users/:id
+const getUserByIdHandler = (req, res) => {
+  const id = req.url.split('/')[3];
+  // console.log(id);
+  const user = users.find((user) => user.id === parseInt(id));
+  res.setHeader('Content-Type', 'application/json');
 
+  if (user) {
+    res.write(JSON.stringify(user));
+    
   } else {
-    res.setHeader('Content-Type', 'application/json');
     res.statusCode = 404;
-    res.write(JSON.stringify({ message: 'Route not found' }));
-    res.end();
+    res.write(JSON.stringify({ message: `User ${id} not found` }));
 
   }
+
+  res.end();
+}
+
+// Not Found handler
+const notFoundHandler = (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.statusCode = 404;
+  res.write(JSON.stringify({ message: 'Route not found' }));
+  res.end();
+}
+const server = createServer((req, res) => {
+
+  logger(req, res, () => {
+    jsonMiddleware(req, res, () => {
+      if (req.url === '/api/users' && req.method === 'GET') {
+        getUserHandler(req, res);
+      } else if (
+        req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'
+      ) {
+        getUserByIdHandler(req, res);
+      } else {
+        notFoundHandler(req, res);
+      }
+    })
+  });
+
 });
 
 server.listen(PORT, () => {
